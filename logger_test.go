09ca90caae567e262b9a064b47b15a55c5ea9e62
phev8 <phev8@users.noreply.github.com>
2021-10-05 14:16:04 +0200

    added unit tests to check log level setting

---
+++ logger_test.go
@@ -0,0 +1,199 @@
+package logger_test
+
+import (
+	"testing"
+
+	"github.com/coneno/logger"
+)
+
+func TestErrorLevel(t *testing.T) {
+	logger.SetLevel(logger.LEVEL_ERROR)
+
+	t.Run("with error output", func(t *testing.T) {
+		n, err := logger.Error.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with warning output", func(t *testing.T) {
+		n, err := logger.Warning.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with info output", func(t *testing.T) {
+		n, err := logger.Info.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with debug output", func(t *testing.T) {
+		n, err := logger.Debug.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+}
+
+func TestWarningLevel(t *testing.T) {
+	logger.SetLevel(logger.LEVEL_WARNING)
+
+	t.Run("with error output", func(t *testing.T) {
+		n, err := logger.Error.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with warning output", func(t *testing.T) {
+		n, err := logger.Warning.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with info output", func(t *testing.T) {
+		n, err := logger.Info.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with debug output", func(t *testing.T) {
+		n, err := logger.Debug.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+}
+
+func TestInfoLevel(t *testing.T) {
+	logger.SetLevel(logger.LEVEL_INFO)
+
+	t.Run("with error output", func(t *testing.T) {
+		n, err := logger.Error.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with warning output", func(t *testing.T) {
+		n, err := logger.Warning.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with info output", func(t *testing.T) {
+		n, err := logger.Info.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with debug output", func(t *testing.T) {
+		n, err := logger.Debug.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 0 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+}
+
+func TestDebugLevel(t *testing.T) {
+	logger.SetLevel(logger.LEVEL_DEBUG)
+
+	t.Run("with error output", func(t *testing.T) {
+		n, err := logger.Error.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with warning output", func(t *testing.T) {
+		n, err := logger.Warning.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with info output", func(t *testing.T) {
+		n, err := logger.Info.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+
+	t.Run("with debug output", func(t *testing.T) {
+		n, err := logger.Info.Writer().Write([]byte("testmsg\n"))
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if n != 8 {
+			t.Errorf("unexpected number of bytes written: %d", n)
+			return
+		}
+	})
+}
